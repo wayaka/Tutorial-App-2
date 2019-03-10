b60990b83814f5df59fa915274323dcc553c64f9
wayaka <suima4743@gmail.com>
2019-03-10 15:33:08 +0000

    Finish user edit index destroy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,10 @@ module SessionsHelper
     end
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def logged_in?
     !current_user.nil?
   end
@@ -17,6 +21,15 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
   
 end
